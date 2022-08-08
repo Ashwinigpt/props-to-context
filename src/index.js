c25ce5f7d70f81ec4730c8adbdd3c0ef0572711b
Ashwini Gupta <ashwinigpt1996@gmail.com>
2022-08-08 10:49:49 +0530

    make with context API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom";
+import { UserContect } from "./UserContext";
 
 import App from "./App";
 
@@ -8,7 +9,9 @@ const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <Router>
-      <App />
+      <UserContect.Provider value={user}>
+        <App />
+      </UserContect.Provider>
     </Router>
   </StrictMode>,
   rootElement
